84ed73cd07ffc38611122a5eec62716f75fd2f25
fernandogross06 <80482002+fernandogross06@users.noreply.github.com>
2025-03-17 20:41:40 -0600

    Cambios rapidos
    cambios rapidos de prueba, se va a cambiar mañana

--- FrontEnd/proyecto-komuness/src/App.js
+++ FrontEnd/proyecto-komuness/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style = {{ width: '200px', height: 'auto' }} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
