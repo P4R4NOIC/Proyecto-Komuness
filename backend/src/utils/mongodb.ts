6369a47d7d236d3031509fbb4c151016573a983d
Willvillegas <eduardo78villegas@estudiantec.cr>
2025-05-07 23:23:57 -0600

    feat(mongodb): enhance connectBD function with connection options and improve formatting

--- backend/src/utils/mongodb.ts
+++ backend/src/utils/mongodb.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-export async function connectBD(url:string): Promise<void> {
+export async function connectBD(url: string): Promise<void> {
     // Connect to MongoDB
     if (url === '') {
         console.log('No se ha especificado una URL para la base de datos');
@@ -7,7 +7,10 @@ export async function connectBD(url:string): Promise<void> {
     }
     try {
 
-        await mongoose.connect(url);
+        await mongoose.connect(url, {
+            serverSelectionTimeoutMS: 30000, // 30 segundos
+            connectTimeoutMS: 30000,
+        });
     } catch (error) {
         console.error(error);
         console.log('Error connecting to MongoDB');
