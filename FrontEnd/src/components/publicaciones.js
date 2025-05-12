e382bf77fb9ab53aeaa91ac6dbc80a062a66fe56
Willvillegas <eduardo78villegas@estudiantec.cr>
2025-05-12 00:26:22 -0600

    refactor(publicaciones): improve pagination logic and fetch handling
    Refactor the pagination logic in the publicaciones component to handle pagination state more effectively. Update the fetch logic to include pagination details from the API response. Simplify the routing logic and improve code readability by removing redundant comments and unused code.
    
    Arreglado:
    🟢 el id por los nombres de las personas en las publicaciones
    🟢 en el backend el sistema de paginación que no se había colocado de forma correcta.
    
    Falta por implemetar:
    🔴 Busquedas y filtros de los archivos.

--- FrontEnd/src/components/publicaciones.js
+++ FrontEnd/src/components/publicaciones.js
@@ -1,141 +1,127 @@
-import React, {useEffect, useState} from 'react'
-import {useLocation} from 'react-router-dom'
+import React, { useEffect, useState, useRef } from 'react' // Añadir useRef
+import { useLocation } from 'react-router-dom'
 import '../CSS/publicaciones.css'
-import PublicacionCard from './publicacionCard';
+import PublicacionCard from './publicacionCard'
 
-// const publicaciones = [
-//     { _id: 1, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 2, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 3, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 4, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 5, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 6, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 7, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-//     { _id: 8, title: 'Fiesta Patronal', image: '/imagenEjemplo.jpg', date: '20/3/2025', tag: 'evento' },
-
-//     { _id: 9, usuario: 'River Okay', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 10, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 11, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 12, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 13, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 14, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 15, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-//     { _id: 16, usuario: 'Juan Perez', post: "Lorem ipsum dolor sit amet", date: '20/3/2025', tag: 'post' },
-
-//     { _id: 17, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 18, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 19, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 20, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 21, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 22, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 23, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-//     { _id: 24, title: 'Venta de zapatos', image: '/imagenEjemplo2.jpg', date: '20/3/2025', tag: 'empr' },
-
-   
-//     ]; 
-  //   {
-  //     "_id": "67dbb128624d67fc3db90c02",
-  //     "titulo": "Locuron el partido no?",
-  //     "contenido": "Que golazo de mi hermano juas juas juas",
-  //     "autor": "67da43f3651480413241b33c",
-  //     "fecha": "Tue Mar 18 2025 06:00:00 GMT-0600 (hora estándar central)",
-  //     "adjunto": [],
-  //     "tag": "publicacion",
-  //     "comentarios": [],
-  //     "__v": 0
-  // }
 
 export const Publicaciones = () => {
-    const location = useLocation();
-    const [mostrar, setMostrar] = useState(0);
-    const [cards, setCards] = useState([]);
-    const [paginaActual, setPaginaActual] = useState(0);
-    const limite = 10; // Definimos cuántas publicaciones por página
-    
-   
+  const location = useLocation();
+  const [publicaciones, setPublicaciones] = useState([]);
+  const [paginacion, setPaginacion] = useState({
+    offset: 0,
+    limit: 10,
+    total: 0,
+    pages: 0
+  })
 
-   
-      
-    const [publicaciones, setPublicaciones] = useState([]);
+  // Definir la referencia para trackear la ruta anterior
+  const prevPath = useRef(location.pathname)
 
-      useEffect(() => {
-        const obtenerPublicaciones = async (tag, offset = 0, limit = 10) => {
-          try {
-            const response = await fetch(`https://proyecto-komuness-backend.vercel.app/publicaciones/?tag=${tag}&offset=${offset}&limit=${limit}`);
-            const data = await response.json();
-            setPublicaciones(data); // Guardamos las publicaciones en el estado
-            console.log("Publicaciones obtenidas:", data);
-          } catch (error) {
-            console.error("Error al obtener publicaciones:", error);
-          }
-        };
-        // Check the current path and set 'mostrar' accordingly
-        if (location.pathname === '/eventos') {
-          setMostrar(0);
-          obtenerPublicaciones('evento', paginaActual * limite, limite);
-        } else if (location.pathname === '/emprendimientos') {
-          setMostrar(1);
-          obtenerPublicaciones('emprendimiento', paginaActual * limite, limite);
-        } else if (location.pathname === '/publicaciones') {
-          setMostrar(2);
-          obtenerPublicaciones('publicacion', paginaActual * limite, 20);
-        } else if (location.pathname === '/perfilUsuario') {
-          setMostrar(3);
-        }
-      }, [location.pathname, paginaActual]);
+  useEffect(() => {
+    let limitePorRuta = 10
+    if (location.pathname === '/publicaciones') limitePorRuta = 10
 
-      
-   
-      useEffect(() => {
-        if (mostrar === 3) {
-          setCards(publicaciones);
-        } else {
-          const newCards = publicaciones.filter(publicacion => {
-            if (mostrar === 0) return publicacion.tag === 'evento';
-            if (mostrar === 1) return publicacion.tag === 'emprendimiento';
-            return publicacion.tag === 'publicacion';
-          });
-      
-          setCards(newCards);
-        }
-      }, [mostrar, publicaciones]); 
-      
-      const paginacion = (valor) => {
-        // Calculamos la nueva página actual
-        setPaginaActual((prev) => Math.max(prev + valor, 0)); // Nunca permitirá una página negativa
-      };
+    const obtenerPublicaciones = async (tag) => {
+      try {
+        // modo de prueba local
+        const url = new URL("http://localhost:3000/publicaciones");
+        // modo de prueba en la nube
+        // const url = new URL("https://proyecto-komuness-backend.vercel.app/publicaciones");
+        url.searchParams.set('tag', tag);
+        url.searchParams.set('offset', paginacion.offset);
+        url.searchParams.set('limit', limitePorRuta);
+        console.log(url)
+        const response = await fetch(new Request(url));
+        const responseData = await response.json()
+        console.log(responseData)
+        setPublicaciones(responseData.data)
+        setPaginacion(prev => ({
+          ...prev,
+          limit: limitePorRuta,
+          ...responseData.pagination
+        }))
+
+      } catch (error) {
+        console.error("Error al obtener publicaciones:", error)
+      }
+    }
+
+    // Resetear paginación solo cuando cambia la ruta
+    if (location.pathname !== prevPath.current) {
+      setPaginacion(prev => ({
+        ...prev,
+        offset: 0,
+        limit: limitePorRuta
+      }))
+      prevPath.current = location.pathname // Actualizar la referencia
+    }
+
+    const routeConfig = {
+      '/eventos': { tag: 'evento' },
+      '/emprendimientos': { tag: 'emprendimiento' },
+      '/publicaciones': { tag: 'publicacion' },
+      '/perfilUsuario': { tag: null }
+    }
+
+    const config = routeConfig[location.pathname] || {}
+
+    if (config.tag) {
+      obtenerPublicaciones(config.tag)
+    }
+    //este useEffect se ejecuta cada vez que cambia la ruta o el offset 
+    //para obtener las publicaciones de la API
+  }, [location.pathname, paginacion.offset])
+
+
+  const handlePagina = (nuevaPagina) => {
+    setPaginacion(prev => ({
+      ...prev,
+      offset: nuevaPagina * prev.limit
+    }))
+  }
+
+  const currentPage = Math.floor(paginacion.offset / paginacion.limit)
 
   return (
     <div className="card-container">
-        {/* {cards} */}
-        {cards.length === 0 ? (
-          <p>No hay publicaciones para mostrar.</p>
-        ) : (
-          cards.map((publicacion) => (
-            <PublicacionCard key={publicacion._id} publicacion={publicacion} />
-          ))
-        )}
-        {/* <div className="paginacion mt-4 flex justify-center gap-4">
+      {publicaciones.length === 0 ? (
+        <p>No hay publicaciones para mostrar.</p>
+      ) : (
+        publicaciones.map((publicacion) => (
+          <PublicacionCard key={publicacion._id} publicacion={publicacion} />
+        ))
+      )}
+
+      {paginacion.pages > 1 && (
+        <div className="paginacion mt-4 flex justify-center gap-4">
           <button
-            onClick={() => paginacion(-1)} // Reduce la página en 1
-            disabled={paginaActual === 0} // Deshabilita el botón si estamos en la página 0
+            onClick={() => handlePagina(currentPage - 1)}
+            disabled={currentPage === 0}
             className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
           >
             Anterior
           </button>
 
-          <span>Página {paginaActual + 1}</span> 
+          <span>
+            Página {currentPage + 1} de {paginacion.pages}
+            <br />
+            <small>
+              Mostrando {paginacion.offset + 1}-{
+                Math.min(paginacion.offset + paginacion.limit, paginacion.total)
+              } de {paginacion.total}
+            </small>
+          </span>
 
           <button
-            onClick={() => paginacion(1)} // Aumenta la página en 1
-            className="px-4 py-2 bg-gray-300 rounded"
+            onClick={() => handlePagina(currentPage + 1)}
+            disabled={currentPage >= paginacion.pages - 1}
+            className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
           >
             Siguiente
           </button>
-        </div> */}
+        </div>
+      )}
     </div>
-
-    
   )
 }
 
--- backend/src/controllers/publicacion.controller.ts
+++ backend/src/controllers/publicacion.controller.ts
@@ -72,22 +72,35 @@ export const getPublicaciones = async (req: Request, res: Response): Promise<voi
 //obtener publicaciones por tag
 export const getPublicacionesByTag = async (req: Request, res: Response): Promise<void> => {
     try {
-
         //paginación
         const offset = parseInt(req.query.offset as string) || 0;
         const limit = parseInt(req.query.limit as string) || 10;
 
         const { tag } = req.query;
-        console.log("Tag recibido:", tag);
-        const publicaciones: IPublicacion[] = await modelPublicacion.find({ tag: tag })
-            .sort({ createdAt: -1 })
-            .skip(offset)
-            .limit(limit);
+
+        const [publicaciones, totalPublicaciones] = await Promise.all([
+            modelPublicacion.find({ tag: tag })
+                .populate('autor', 'nombre') // Agregar populate para obtener el nombre del autor
+                .sort({ createdAt: -1 })
+                .skip(offset)
+                .limit(limit),
+            modelPublicacion.countDocuments({ tag: tag })
+        ]);
+
         if (publicaciones.length === 0) {
             res.status(404).json({ message: 'No se encontraron publicaciones con ese tag' });
             return;
         }
-        res.status(200).json(publicaciones);
+
+        res.status(200).json({
+            data: publicaciones,
+            pagination: {
+                offset,
+                limit,
+                total: totalPublicaciones,
+                pages: Math.ceil(totalPublicaciones / limit),
+            },
+        });
     } catch (error) {
         const err = error as Error;
         res.status(500).json({ message: err.message });
